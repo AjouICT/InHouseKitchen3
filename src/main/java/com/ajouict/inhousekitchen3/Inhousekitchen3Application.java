baf5ac218392c401da8df20fb314b2b733c602d3
JeongHo <wwwkang8@gmail.com>
2018-09-20 00:21:52 +0900

    initial commit

---
+++ src/main/java/com/ajouict/inhousekitchen3/Inhousekitchen3Application.java
@@ -0,0 +1,12 @@
+package com.ajouict.inhousekitchen3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inhousekitchen3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Inhousekitchen3Application.class, args);
+    }
+}
